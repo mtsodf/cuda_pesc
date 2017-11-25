6a5280574ec6f43b2c2cba2d9510a948c38db9e9
mtsodf <mtsodf@gmail.com>
2017-11-25 18:47:31 -0200

    Alocando memoria do caso de dupla precisao

--- pycuda/GaussSeidel.py
+++ pycuda/GaussSeidel.py
@@ -54,16 +54,16 @@ def MatVecSp(A, b):
 
     n = np.int32(b.size)
 
+
+    b_sp = b.astype(np.float32)
     data_sp = A.data.astype(np.float32)
+
+    # Alocando memoria na gpu
     ia_gpu = cuda.mem_alloc(A.indptr.nbytes)
     ja_gpu = cuda.mem_alloc(A.indices.nbytes)
     data_gpu = cuda.mem_alloc(data_sp.nbytes)
-
-    b_sp = b.astype(np.float32)
-
     b_gpu = cuda.mem_alloc(b_sp.nbytes)
     c_gpu = cuda.mem_alloc(b_sp.nbytes)
-
 
 
     cuda.memcpy_htod(ia_gpu, A.indptr)
@@ -97,7 +97,20 @@ def MatVecSp(A, b):
 def MatVec(A, b):
 
     n = np.int32(b.size)
-    c = np.zeros(n)
+
+
+    # Alocando memoria na gpu
+    ia_gpu = cuda.mem_alloc(A.indptr.nbytes)
+    ja_gpu = cuda.mem_alloc(A.indices.nbytes)
+    data_gpu = cuda.mem_alloc(A.data.nbytes)
+    b_gpu = cuda.mem_alloc(b.nbytes)
+    c_gpu = cuda.mem_alloc(b.nbytes)
+
+    cuda.memcpy_htod(ia_gpu, A.indptr)
+    cuda.memcpy_htod(ja_gpu, A.indices)
+    cuda.memcpy_htod(data_gpu, A.data)
+    cuda.memcpy_htod(b_gpu, b)
+
 
     blocksize = 64
 
@@ -109,10 +122,15 @@ def MatVec(A, b):
     grid = (gridsize, 1, 1)
     block = (blocksize, 1, 1)
 
-    func(cuda.InOut(A.indptr), cuda.InOut(A.indices), cuda.InOut(A.data), cuda.InOut(b), cuda.InOut(c), n,
+    t1 = time()
+    func(ia_gpu, ja_gpu, data_gpu, b_gpu, c_gpu, n,
         grid=grid, block=block)
+    pycuda.driver.Context.synchronize()
+    t2 = time()
 
-    return c
+    c = np.empty_like(b)
+    cuda.memcpy_dtoh(c, c_gpu)
+    return c, t2-t1
 
 n = np.int32(10000000)
 
@@ -121,19 +139,20 @@ A = identity(n, format='csr')
 b = np.ones(n)
 
 t1 = time()
-c = MatVec(A, b)
+c1, dt1 = MatVec(A, b)
 t2 = time()
 d = A*b
 t3 = time()
-c, dt = MatVecSp(A,b)
+c2, dt2 = MatVecSp(A,b)
 t4 = time()
 
-print "Norma da Diferenca = ", np.linalg.norm(d-c)
+print "Norma da Diferenca = ", np.linalg.norm(c1-d)
+print "Norma da Diferenca = ", np.linalg.norm(c2-d)
 
 
-print "Tempo Numpy SP = ", t4-t3, dt
+print "Tempo Numpy SP = ", t4-t3, dt2
 
-print "Tempo Gpu DP = ", t2-t1
+print "Tempo Gpu DP = ", t2-t1, dt1
 
 print "Tempo Numpy  = ", t3-t2
 
